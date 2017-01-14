e591c1089271bbdb7a446a324ec0c35a69695449
noraworld <noraworld@mail.com>
2017-01-14 11:37:49 +0900

    :sparkles: Add reverse sort on sort.py

--- sort.py
+++ sort.py
@@ -1,32 +1,49 @@
 import sys
 
-def sort(arr, dst_splitter):
-    arr = sorted(arr)
-    for i, num in enumerate(arr):
-        if not i == (len(arr) - 1):
-            print(num, end = dst_splitter)
-        else:
-            print(num)
+def sort(arr, dst_splitter, mode):
+    arr.sort()
 
-def check_num(arr, dst_splitter):
+    if mode == 'reverse':
+        arr.reverse()
+
+    output(arr, dst_splitter)
+
+
+def load(filepath, src_splitter, dst_splitter, mode):
+    file = open(filepath, 'r')
+    data = file.read()
+    arr  = data.split(src_splitter)
+    check_num(arr, dst_splitter, mode)
+
+
+def check_num(arr, dst_splitter, mode):
     isnum = True
+
     for i, n in enumerate(arr):
         try:
             arr[i] = int(n)
         except:
             print('ERROR: ' + '{0:15s}'.format(repr(arr[i])) + ' is not a number.')
             isnum = False
+
     if isnum == False:
         exit()
-    sort(arr, dst_splitter)
 
-def load(filepath, src_splitter, dst_splitter):
-    file = open(filepath, 'r')
-    data = file.read()
-    arr  = data.split(src_splitter)
-    check_num(arr, dst_splitter)
+    sort(arr, dst_splitter, mode)
+
 
 def check_arg(params):
+    # if reverse option exists
+    if '-r' in params:
+        mode = 'reverse'
+        params.remove('-r')
+    elif '--reverse' in params:
+        mode = 'reverse'
+        params.remove('--reverse')
+    else:
+        mode = 'normal'
+
+    # check arguments
     if len(params) == 4:
         filepath     = params[1]
         src_splitter = params[2]
@@ -42,7 +59,20 @@ def check_arg(params):
     else:
         print('Invaild command line arguments')
         exit()
-    load(filepath, src_splitter, dst_splitter)
+
+    # load file
+    load(filepath, src_splitter, dst_splitter, mode)
+
+
+def output(arr, dst_splitter):
+    for i, num in enumerate(arr):
+        if not i == (len(arr) - 1):
+            # output with splitter
+            print(num, end = dst_splitter)
+        else:
+            # for last output
+            print(num)
+
 
 def main():
     params = sys.argv
